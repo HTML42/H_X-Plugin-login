7a3de9dea4566a8a5df5b7ba62912e63c926f3ba
Mark Paspirgilis <service@html42.de>
2018-12-24 15:12:12 +0100

    basic-files

---
+++ classes/xlogin.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class Xlogin {
+    
+    const TEMPLATES = array(
+        'login' => X_LOGIN_PATH . 'templates/login.php',
+        'registration' => X_LOGIN_PATH . 'templates/registration.php',
+    );
+    
+}
