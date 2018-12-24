8c572b8bf5d7305182c606efc9cbb4d48229c00c
Mark Paspirgilis <service@html42.de>
2018-12-24 22:15:04 +0100

    xlogin script include

--- script_includes/xplugin_login.php
+++ script_includes/xplugin_login.php
@@ -1,11 +1,10 @@
 <?php
 
-#Variables
-define('X_LOGIN_PATH', PROJECT_ROOT . 'plugins/login/');
+include PROJECT_ROOT . 'plugins/login/kickstart.php';
 
-#Files
-foreach(array('classes/xlogin.class.php', 'classes/xuser.class.php') as $plugin_file_path) {
-    if(is_file(X_LOGIN_PATH . $plugin_file_path)) {
-        include X_LOGIN_PATH . $plugin_file_path;
-    }
-}
+
+#Start Database
+$XLDB = new Xlogin_DB();
+
+#Create Current User
+$Xme = new Xuser(X_LOGIN_ID);
