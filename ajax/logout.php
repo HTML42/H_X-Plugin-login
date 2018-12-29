c4943622a94ef109f0c098fc6e2636708b8e5dbb
Mark Paspirgilis <service@html42.de>
2018-12-29 23:32:26 +0100

    enhancement

--- ajax/logout.php
+++ ajax/logout.php
@@ -1,10 +1,12 @@
 <?php
 
-include '../../../xtreme/library/bootstrap.php';
+if (is_file('../../../xtreme/library/bootstrap.php')) {
+    include_once '../../../xtreme/library/bootstrap.php';
+}
 
 Xlogin::session('userid', 0);
 
-if(isset($_GET['redirect']) && is_string($_GET['redirect'])) {
+if (isset($_GET['redirect']) && is_string($_GET['redirect'])) {
     Utilities::redirect(Utilities::validate($_GET['redirect']));
 }
 
